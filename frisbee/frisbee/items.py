23af64fe142f8fa41809c1f5fa45d5bfdce3bc70
Mark Shannon <mts0040@auburn.edu>
2019-06-05 13:03:36 -0500

    Get the teams in each tournament

--- frisbee/frisbee/items.py
+++ frisbee/frisbee/items.py
@@ -9,11 +9,20 @@ import scrapy
 from scrapy.item import Item, Field
 from scrapy import Spider
 
-
+# contains all the information I will need
 class FrisbeeItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
 
     tournament_name = Field()
     tournament_url = Field()
+    tournament_teams = Field()
+
+# Will be a list containing the team names and twitters
+class TeamListItem(scrapy.Item):
+    teams = Field()
+
+class team(scrapy.Item):
+    team_name = Field()
+    team_twitter = Field()
 
